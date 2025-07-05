b18387ce5782001d529b5046f0edc24fe3c225a9
ksenyay <ksenyausachova@gmail.com>
2025-07-05 20:06:45 +0300

    Added icons to the task card

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -12,7 +12,11 @@ const Board: React.FC<BoardProps> = ({ column }) => {
       <h2>{column}</h2>
     </div>
       <div className={styles.cardContainer}>
-        <div className={styles.card} draggable>sdasd</div>
+        <div className={styles.card} draggable>
+          <h3>title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, doloribus enim esse ad illo expl.</p>
+          <div className={styles.iconContainer}><img className={styles.icon} src="edit.png" alt="" /><img className={styles.icon} src="delete.png" alt="" /></div>
+        </div>
         <button className={styles.addButton}>+</button>
       </div>
 
