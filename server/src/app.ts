1d2be10d84bca792ccff81d5d595e3f344d96561
ksenyay <ksenyausachova@gmail.com>
2025-07-08 15:54:55 +0300

    Added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import boardRoutes from './routes/boardRoutes';
 import cardRoutes from './routes/cardRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -10,8 +11,7 @@ const app: Application = express();
 const PORT: number = Number(process.env.PORT) || 3000;
 const MONGODB_URI: string = process.env.MONGODB_URI || '';
 
-console.log('MONGODB_URI =', MONGODB_URI);
-
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
